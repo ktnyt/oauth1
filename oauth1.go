3eb46a8463790b0db36a162ea9b003ff9b7a2fc4
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-03-22 18:07:29 +0900

    Add all but reference testing

---
+++ oauth1.go
@@ -0,0 +1,276 @@
+package oauth1
+
+import (
+	"bytes"
+	"context"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/ktnyt/oauth1/internal"
+)
+
+// NoContext is the default context you should supply if not using
+// your own context.Context (see https://golang.org/x/net/context).
+//
+// Deprecated: Use context.Background() or context.TODO() instead.
+var NoContext = context.TODO()
+
+// Config describes a typical OAuth1 flow, given a Consumer Key,
+// Consumer Secret, and a Callback URL.
+type Config struct {
+	// Consumer Key (Client Identifier)
+	ConsumerKey string
+
+	// Consumer Secret (Client Shared-Secret)
+	ConsumerSecret string
+
+	// Callback URL
+	CallbackURL string
+
+	// Provider Endpoint specifying OAuth1 endpoint URLs
+	Endpoint Endpoint
+}
+
+// Endpoint contains the OAuth 1.0 provider's request token,
+// authorization, and access token URLs.
+type Endpoint struct {
+	// Request URL (Temporary Credential Request URI)
+	RequestTokenURL string
+
+	// Authorize URL (Resource Owner Authorization URI)
+	AuthorizeURL string
+
+	// Access Token URL (Token Request URI)
+	AccessTokenURL string
+}
+
+// Client returns an HTTP client using the provided access tokens.
+// HTTP transport will be obtained using the provided context.
+// The returned client and its Transport should not be modified.
+func (c *Config) Client(ctx context.Context, accessToken, accessSecret string) *http.Client {
+	return NewClient(ctx, c.ConsumerKey, c.ConsumerSecret, accessToken, accessSecret)
+}
+
+// RequestToken obtains a Request token and secret (temporary credential) by
+// POSTing a request (with oauth_callback in the auth header) to the Endpoint
+// RequestTokenURL. The response body form is validated to ensure
+// oauth_callback_confirmed is true. Returns the request token and secret
+// (temporary credentials).
+// See RFC 5849 2.1 Temporary Credentials.
+func (c *Config) RequestToken() (string, string, error) {
+	// Setup to request a request_token pair
+	req, err := http.NewRequest("POST", c.Endpoint.RequestTokenURL, nil)
+	if err != nil {
+		return "", "", err
+	}
+	params, err := prepareParams(req, c.ConsumerKey)
+	if err != nil {
+		return "", "", err
+	}
+	params.Add("oauth_callback", c.CallbackURL)
+	signature, err := sign(c.ConsumerSecret, "", req, params)
+	if err != nil {
+		return "", "", err
+	}
+	params.Add("oauth_signature", signature)
+	req.Header.Add("Authorization", formatOAuthHeader(params))
+
+	// Request a request_token pair
+	res, err := internal.ContextClient(nil).Do(req)
+	if err != nil {
+		return "", "", err
+	}
+	defer res.Body.Close()
+
+	// Handle request_token response
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated {
+		return "", "", fmt.Errorf("oauth1: Server returned unexpected status %d", res.StatusCode)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", "", err
+	}
+	values, err := url.ParseQuery(string(body))
+	if err != nil {
+		return "", "", err
+	}
+	requestToken := values.Get("oauth_token")
+	requestSecret := values.Get("oauth_token_secret")
+	if requestToken == "" || requestSecret == "" {
+		return "", "", errors.New("oauth1: Response missing oauth_token or oauth_token_secret")
+	}
+	if values.Get("oauth_callback_confirmed") != "true" {
+		return "", "", errors.New("oauth1: oauth_callback_confirmed was not true")
+	}
+	return requestToken, requestSecret, nil
+}
+
+// AuthorizationURL accepts a request token and returns the *url.URL to the
+// Endpoint's authorization page that asks the user (resource owner) for to
+// authorize the consumer to act on his/her/its behalf.
+// See RFC 5849 2.2 Resource Owner Authorization.
+func (c *Config) AuthorizationURL(requestToken string) (*url.URL, error) {
+	authorizationURL, err := url.Parse(c.Endpoint.AuthorizeURL)
+	if err != nil {
+		return nil, err
+	}
+	values := authorizationURL.Query()
+	values.Add("oauth_token", requestToken)
+	authorizationURL.RawQuery = values.Encode()
+	return authorizationURL, nil
+}
+
+// ParseAuthorizationCallback parses an OAuth1 authorization callback request
+// from a provider server. The oauth_token and oauth_verifier parameters are
+// parsed to return the request token from earlier in the flow and the
+// verifier string.
+// See RFC 5849 2.2 Resource Owner Authorization.
+func ParseAuthorizationCallback(req *http.Request) (string, string, error) {
+	err := req.ParseForm()
+	if err != nil {
+		return "", "", err
+	}
+	requestToken := req.Form.Get("oauth_token")
+	verifier := req.Form.Get("oauth_verifier")
+	if requestToken == "" || verifier == "" {
+		return "", "", errors.New("oauth1: Request missing oauth_token or oauth_verifier")
+	}
+	return requestToken, verifier, nil
+}
+
+// AccessToken obtains an access token (token credential) by POSTing a
+// request (with oauth_token and oauth_verifier in the auth header) to the
+// Endpoint AccessTokenURL. Returns the access token and secret (token
+// credentials).
+// See RFC 5849 2.3 Token Credentials.
+func (c *Config) AccessToken(requestToken, requestSecret, verifier string) (string, string, error) {
+	// Setup to request an access_token pair
+	req, err := http.NewRequest("POST", c.Endpoint.AccessTokenURL, nil)
+	if err != nil {
+		return "", "", err
+	}
+	params, err := prepareParams(req, c.ConsumerKey)
+	if err != nil {
+		return "", "", err
+	}
+	params.Add("oauth_token", requestToken)
+	params.Add("oauth_verifier", verifier)
+	signature, err := sign(c.ConsumerSecret, "", req, params)
+	if err != nil {
+		return "", "", err
+	}
+	params.Add("oauth_signature", signature)
+	req.Header.Add("Authorization", formatOAuthHeader(params))
+
+	// Request an access_token pair
+	res, err := internal.ContextClient(nil).Do(req)
+	if err != nil {
+		return "", "", err
+	}
+	defer res.Body.Close()
+
+	// Handle access_token response
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated {
+		return "", "", fmt.Errorf("oauth1: Server returned unexpected status %d", res.StatusCode)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", "", err
+	}
+	values, err := url.ParseQuery(string(body))
+	if err != nil {
+		return "", "", err
+	}
+	accessToken := values.Get("oauth_token")
+	accessSecret := values.Get("oauth_token_secret")
+	if accessToken == "" || accessSecret == "" {
+		return "", "", errors.New("oauth1: Response missing oauth_token or oauth_token_secret")
+	}
+	return accessToken, accessSecret, nil
+}
+
+// HTTPClient is the context key to use with 's WithValue function
+// to associate an *http.Client value with a context.
+var HTTPClient internal.ContextKey
+
+// NewClient creates an *http.Client from a Context and tokens.
+// The returned client is not valid beyond the lifetime of the context.
+//
+// Note that if a custom *http.Client is provided via the Context it
+// is used only for token acquisition and is not used to configure the
+// *http.Client returned from NewClient.
+func NewClient(ctx context.Context, consumerKey, consumerSecret, accessToken, accessSecret string) *http.Client {
+	return &http.Client{
+		Transport: &Transport{
+			Base:           internal.ContextClient(ctx).Transport,
+			consumerKey:    consumerKey,
+			consumerSecret: consumerSecret,
+			accessToken:    accessToken,
+			accessSecret:   accessSecret,
+		},
+	}
+}
+
+func nonce() string {
+	h := md5.New()
+	now := time.Now().Unix()
+	io.WriteString(h, strconv.FormatInt(now, 10))
+	io.WriteString(h, strconv.FormatInt(rand.Int63(), 10))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func prepareParams(r *http.Request, consumerKey string) (url.Values, error) {
+	params := make(url.Values)
+	if r.Body != nil && r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return params, err
+		}
+		params, err = url.ParseQuery(string(b))
+		if err != nil {
+			return params, err
+		}
+		r.Body = ioutil.NopCloser(bytes.NewReader(b))
+	}
+	params.Add("oauth_consumer_key", consumerKey)
+	params.Add("oauth_nonce", nonce())
+	params.Add("oauth_signature_method", "HMAC-SHA1")
+	params.Add("oauth_timestamp", strconv.FormatInt(time.Now().Unix(), 10))
+	params.Add("oauth_version", "1.0")
+	return params, nil
+}
+
+func sign(consumerSecret, tokenSecret string, req *http.Request, params url.Values) (string, error) {
+	upperMethod := strings.ToUpper(req.Method)
+	escapedURL := url.QueryEscape(req.URL.String())
+	escapedParams := url.QueryEscape(params.Encode())
+	base := strings.Join([]string{upperMethod, escapedURL, escapedParams}, "&")
+	key := strings.Join([]string{consumerSecret, tokenSecret}, "&")
+	h := hmac.New(sha1.New, []byte(key))
+	if _, err := h.Write([]byte(base)); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(h.Sum(nil)), nil
+}
+
+func formatOAuthHeader(params url.Values) string {
+	joined := params.Encode()
+	pairs := strings.Split(joined, "&")
+	for i := range pairs {
+		pair := strings.Split(pairs[i], "=")
+		pairs[i] = fmt.Sprintf("%s=\"%s\"", pair[0], pair[1])
+	}
+	return fmt.Sprintf("OAuth %s", strings.Join(pairs, ", "))
+}
---
+++ oauth1_test.go
@@ -0,0 +1,219 @@
+package oauth1
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const expectedVerifier = "some_verifier"
+
+func TestNewClient(t *testing.T) {
+	expectedToken := "access_token"
+	expectedConsumerKey := "consumer_key"
+	config := Config{ConsumerKey: expectedConsumerKey, ConsumerSecret: "consumer_secret"}
+	client := config.Client(NoContext, expectedToken, "access_secret")
+
+	server := newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "GET", req.Method)
+		params := parseOAuthParamsOrFail(t, req.Header.Get("Authorization"))
+		assert.Equal(t, expectedToken, params["oauth_token"])
+		assert.Equal(t, expectedConsumerKey, params["oauth_consumer_key"])
+	})
+	defer server.Close()
+	client.Get(server.URL)
+}
+
+func TestNewClient_DefaultTransport(t *testing.T) {
+	config := &Config{ConsumerKey: "t", ConsumerSecret: "s"}
+	client := NewClient(NoContext, config.ConsumerKey, config.ConsumerSecret, "t", "s")
+	// assert that the client uses the DefaultTransport
+	transport, ok := client.Transport.(*Transport)
+	assert.True(t, ok)
+	assert.Equal(t, http.DefaultTransport, transport.base())
+}
+
+func TestNewClient_ContextClientTransport(t *testing.T) {
+	baseTransport := &http.Transport{}
+	baseClient := &http.Client{Transport: baseTransport}
+	ctx := context.WithValue(NoContext, HTTPClient, baseClient)
+	config := &Config{ConsumerKey: "t", ConsumerSecret: "s"}
+	client := NewClient(ctx, config.ConsumerKey, config.ConsumerSecret, "t", "s")
+	// assert that the client uses the ctx client's Transport as its base RoundTripper
+	transport, ok := client.Transport.(*Transport)
+	assert.True(t, ok)
+	assert.Equal(t, baseTransport, transport.base())
+}
+
+// newRequestTokenServer returns a new httptest.Server for an OAuth1 provider
+// request token endpoint.
+func newRequestTokenServer(t *testing.T, data url.Values) *httptest.Server {
+	return newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "POST", req.Method)
+		assert.NotEmpty(t, req.Header.Get("Authorization"))
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(data.Encode()))
+	})
+}
+
+// newAccessTokenServer returns a new httptest.Server for an OAuth1 provider
+// access token endpoint.
+func newAccessTokenServer(t *testing.T, data url.Values) *httptest.Server {
+	return newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "POST", req.Method)
+		assert.NotEmpty(t, req.Header.Get("Authorization"))
+		params := parseOAuthParamsOrFail(t, req.Header.Get("Authorization"))
+		assert.Equal(t, expectedVerifier, params["oauth_verifier"])
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(data.Encode()))
+	})
+}
+
+// newUnparseableBodyServer returns a new httptest.Server which writes
+// responses with bodies that error when parsed by url.ParseQuery.
+func newUnparseableBodyServer() *httptest.Server {
+	return newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		// url.ParseQuery will error, https://golang.org/src/net/url/url_test.go#L1107
+		w.Write([]byte("%gh&%ij"))
+	})
+}
+
+func TestConfigRequestToken(t *testing.T) {
+	expectedToken := "reqest_token"
+	expectedSecret := "request_secret"
+	data := url.Values{}
+	data.Add("oauth_token", expectedToken)
+	data.Add("oauth_token_secret", expectedSecret)
+	data.Add("oauth_callback_confirmed", "true")
+	server := newRequestTokenServer(t, data)
+	defer server.Close()
+
+	config := &Config{
+		Endpoint: Endpoint{
+			RequestTokenURL: server.URL,
+		},
+	}
+	requestToken, requestSecret, err := config.RequestToken()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedToken, requestToken)
+	assert.Equal(t, expectedSecret, requestSecret)
+}
+
+func TestConfigRequestToken_InvalidRequestTokenURL(t *testing.T) {
+	config := &Config{
+		Endpoint: Endpoint{
+			RequestTokenURL: "http://wrong.com/oauth/request_token",
+		},
+	}
+	requestToken, requestSecret, err := config.RequestToken()
+	assert.NotNil(t, err)
+	assert.Equal(t, "", requestToken)
+	assert.Equal(t, "", requestSecret)
+}
+
+func TestConfigAccessToken_CannotParseBody(t *testing.T) {
+	server := newUnparseableBodyServer()
+	defer server.Close()
+
+	config := &Config{
+		Endpoint: Endpoint{
+			AccessTokenURL: server.URL,
+		},
+	}
+	accessToken, accessSecret, err := config.AccessToken("any_token", "any_secret", "any_verifier")
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "invalid URL escape")
+	}
+	assert.Equal(t, "", accessToken)
+	assert.Equal(t, "", accessSecret)
+}
+
+func TestConfigAccessToken_MissingTokenOrSecret(t *testing.T) {
+	data := url.Values{}
+	data.Add("oauth_token", "any_token")
+	server := newAccessTokenServer(t, data)
+	defer server.Close()
+
+	config := &Config{
+		Endpoint: Endpoint{
+			AccessTokenURL: server.URL,
+		},
+	}
+	accessToken, accessSecret, err := config.AccessToken("request_token", "request_secret", expectedVerifier)
+	if assert.Error(t, err) {
+		assert.Equal(t, "oauth1: Response missing oauth_token or oauth_token_secret", err.Error())
+	}
+	assert.Equal(t, "", accessToken)
+	assert.Equal(t, "", accessSecret)
+}
+
+func TestParseAuthorizationCallback_GET(t *testing.T) {
+	expectedToken := "token"
+	expectedVerifier := "verifier"
+	server := newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "GET", req.Method)
+		// logic under test
+		requestToken, verifier, err := ParseAuthorizationCallback(req)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedToken, requestToken)
+		assert.Equal(t, expectedVerifier, verifier)
+	})
+	defer server.Close()
+
+	// OAuth1 provider calls callback url
+	url, err := url.Parse(server.URL)
+	assert.Nil(t, err)
+	query := url.Query()
+	query.Add("oauth_token", expectedToken)
+	query.Add("oauth_verifier", expectedVerifier)
+	url.RawQuery = query.Encode()
+	http.Get(url.String())
+}
+
+func TestParseAuthorizationCallback_POST(t *testing.T) {
+	expectedToken := "token"
+	expectedVerifier := "verifier"
+	server := newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "POST", req.Method)
+		// logic under test
+		requestToken, verifier, err := ParseAuthorizationCallback(req)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedToken, requestToken)
+		assert.Equal(t, expectedVerifier, verifier)
+	})
+	defer server.Close()
+
+	// OAuth1 provider calls callback url
+	form := url.Values{}
+	form.Add("oauth_token", expectedToken)
+	form.Add("oauth_verifier", expectedVerifier)
+	http.PostForm(server.URL, form)
+}
+
+func TestParseAuthorizationCallback_MissingTokenOrVerifier(t *testing.T) {
+	server := newMockServer(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "GET", req.Method)
+		// logic under test
+		requestToken, verifier, err := ParseAuthorizationCallback(req)
+		if assert.Error(t, err) {
+			assert.Equal(t, "oauth1: Request missing oauth_token or oauth_verifier", err.Error())
+		}
+		assert.Equal(t, "", requestToken)
+		assert.Equal(t, "", verifier)
+	})
+	defer server.Close()
+
+	// OAuth1 provider calls callback url
+	url, err := url.Parse(server.URL)
+	assert.Nil(t, err)
+	query := url.Query()
+	query.Add("oauth_token", "any_token")
+	query.Add("oauth_verifier", "") // missing oauth_verifier
+	url.RawQuery = query.Encode()
+	http.Get(url.String())
+}
